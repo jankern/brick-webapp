eefe3391d1fb5d99574c6521fca6cd03b64f1581
janpkern@googlemail.com <janpkern@googlemail.com>
2021-06-19 15:51:00 +0200

    basic animation with controlls enabled

--- src/js/module.three.js
+++ src/js/module.three.js
@@ -4,13 +4,15 @@
 import * as THREE from "three";
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import * as dat from 'dat.gui';
 
 let camera, scene, renderer;
 let geometry, material, mesh;
 let controls, pointLight, ambientLight, lightHelper, gridHelper;
-let animate;
+let animate, moveCamera, onDocumentMouseDown, meshObjects, intersects;
 
 const gltfLoader = new GLTFLoader();
+const gui = new dat.GUI();
 
 export
     default class Three {
@@ -20,38 +22,74 @@ export
 
     init(){
 
-        console.log(window.innerWidth / window.innerHeight)
+        // let x = 0.01;
+        // let y = 0.09;    
+        // let z = 0.01;
+        let x = -0.01;
+        let y = -0.01;    
+        let z = 0.00;
+        let isCamMoving = false;
 
-        camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
-        camera.position.set(0.0, 0.08, 0.25);
-    
+        camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );    
         scene = new THREE.Scene();
 
-        // let newQuaternion = new THREE.Quaternion();
-        // THREE.Quaternion.slerp(camera.quaternion, 1, 9, 0.07);
-        // camera.quaternion = newQuaternion;
+        // loading model
+        let model;
 
-        // loading modelel
         gltfLoader.load(
             "../assets/gltf/showroom.glb",
             (glb) => {
 
-                // glb.scene.traverse( function( node ) {
-                //     if ( node instanceof THREE.Mesh ) { 
-                //       node.castShadow = true; 
-                //       node.material.side = THREE.DoubleSide;
-                //     }
-                //   });
-
-                const model = glb.scene;
+                model = glb.scene;
                 model.scale.set(1.5,1.9,1.5);
-                model.rotation.x += 0.2;
+                //model.rotation.x += 0.2;
                 model.rotation.y += -0.8;
-                model.position.x = 0.03;
-                model.castShadow = true;
-                model.receiveShadow = true;
-               
+
+                model.position.z = 0.05;
+                model.position.y = -0.05;
+
+                // model.castShadow = true;
+                // model.receiveShadow = true;
+
                 scene.add(model);
+
+                let raycaster = new THREE.Raycaster();
+                let mouse = new THREE.Vector2();
+            
+                onDocumentMouseDown = (event) => {
+                    event.preventDefault();
+            
+                    // mouse.x = (event.clientX / renderer.domElement.clientWidth) * 2 - 1;
+                    // mouse.y =  - (event.clientY / renderer.domElement.clientHeight) * 2 + 1;
+
+                    mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+	                mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+            
+                    raycaster.setFromCamera(mouse, camera);
+            
+                    intersects = raycaster.intersectObjects(scene.children, true);
+
+                    for(let i in intersects){
+                        console.log(intersects[i])
+                    }
+
+                    model.click = () => {
+                        console.log('Tuwas, biddee');
+                        isCamMoving = true;
+                    }
+
+                    const isIntersected = intersects.find(
+                        (intersectedEl) => intersectedEl.object.parent.uuid === model.uuid
+                    );
+        
+                    console.log(isIntersected);
+                    if (isIntersected) {
+                       model.click();
+                    }
+                
+                }
+
+                document.addEventListener('mousedown', onDocumentMouseDown, false);
             },
             (xhr) => {
                 console.log((xhr.loaded/xhr.total * 100) + '%');
@@ -62,20 +100,24 @@ export
         );
 
         let light = new THREE.DirectionalLight(0xffffff);
-        light.position.set(5,5,5);
+        light.position.set(5,2,5);
         scene.add(light);
 
         /* ------------- HELPER ---------------- */
     
-        // pointLight = new THREE.PointLight(pointLight);
-        // pointLight.position.set(5,5,5);
+        // pointLight = new THREE.PointLight(0xffffff);
+        // pointLight.position.set(0.5, 0.5, 0.5);
 
         // ambientLight = new THREE.AmbientLight(0xffffff);
-        // scene.add(pointLight, ambientLight);
+        // pointLight.position.set(0.5,0.5,0.5);   
 
-        // lightHelper = new THREE.PointLightHelper(pointLight);
-        // gridHelper = new THREE.GridHelper(gridHelper);
-        // scene.add(lightHelper, gridHelper);
+        // //scene.add(pointLight, ambientLight);
+        // scene.add(ambientLight);
+
+        //lightHelper = new THREE.PointLightHelper(pointLight);
+        gridHelper = new THREE.GridHelper(gridHelper);
+        //scene.add(lightHelper, gridHelper);
+        scene.add(gridHelper);
 
         /* ------------------------------------- */
     
@@ -83,14 +125,25 @@ export
         renderer.setPixelRatio( window.devicePixelRatio );
         renderer.setSize( window.innerWidth, window.innerHeight ); //
 
-        //controls = new OrbitControls(camera, renderer.domElement);
+        controls = new OrbitControls(camera, renderer.domElement);
+        controls.autoRotate = false;
+        camera.position.set(0.0, 0.08, 0.25);
 
         animate = () => {
             requestAnimationFrame(animate);
-            console.log('/////#######');
-            //model.rotation.x += 0.01;
-            //controls.update();
+            
+            if(isCamMoving && y > -0.12){
+                this.moveCamera(camera, x, y, z);
+                // x -= 0.01;
+                // y -= 0.01;
+                x += 0.01;
+                y -= 0.01;
+                z += 0.02;
+            }
+
+            controls.update();
             renderer.render( scene, camera );  
+
         };
 
         animate();
@@ -101,28 +154,36 @@ export
 
     }
 
-    callRenderer(){
-        console.log('#######');
-        renderer.render( scene, camera );
-    }
+    moveCamera(camera, x, y, z){
+        
+        console.log('x: '+x);
+        console.log('y: '+y);
+        console.log('z: '+z);
+ 
+        // var x = 0.0 - x,
+        // y = 0.08 + x, 
+        // z = 0.25;
 
-    animation(time){
+        var x = 0.0 - x,
+        y = 0.08 + y, 
+        z = 0.26;
 
-        mesh.rotation.x = time / 1500;
-        mesh.rotation.y = time / 1000;
-    
-        renderer.render( scene, camera );
+        //camera.position.set(0.0, 0.08, 0.25);
 
+        // position property can be used to set
+        // the position of a camera
+        camera.position.set(x, y, z);
+        // the rotation property or the lookAt method
+        // can be used to set rotation
+        // camera.lookAt(0, 0, 0);
     }
 
     onWindowResize(){
-
         console.log('resized')
         camera.aspect = window.innerWidth / window.innerHeight;
         camera.updateProjectionMatrix();
         renderer.setSize(window.innerWidth, window.innerHeight);
         renderer.render( scene, camera );
-
     }
 
     out(){
